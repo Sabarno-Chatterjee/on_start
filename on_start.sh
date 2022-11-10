b846444f4b819556bd4447869d349388351a553e
Sabarno Chatterjee <sabarno.05@gmail.com>
2022-11-10 20:08:52 +0530

    Edit on_start.sh

---
+++ on_start.sh
@@ -0,0 +1,42 @@
+  GNU nano 6.2                                                                               on_start.sh                                                                                        
+#!/bin/bash
+
+printf "\n"
+
+date
+
+printf "\n"
+
+sleep 1
+
+printf "\n\n"
+
+echo "Good morning sir! It's time to BASH some shit up"
+
+
+printf "\n\n"
+sleep 2
+
+printf "\n\n"
+echo "Getting weather updates"
+
+sleep 2
+
+printf "\n\n"
+
+curl wttr.in/gurugram
+
+sleep 5
+printf "\n\n\n"
+
+echo "Launching chrome"
+
+sleep 2
+
+google-chrome &
+printf "\n\n"
+
+sleep 7
+printf "\n\n"
+
+echo "Would you like some music? (y/n)"
